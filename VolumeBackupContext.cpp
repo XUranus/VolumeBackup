61fe3a7fbbc85212148b56f98e0a2abef233fe96
XUranus <2257238649wdx@gmail.com>
2023-06-12 22:46:14 +0800

    fix alloc

--- VolumeBackupContext.cpp
+++ VolumeBackupContext.cpp
@@ -16,7 +16,7 @@ VolumeBlockAllocator::VolumeBlockAllocator(uint32_t blockSize, uint32_t blockNum
  : m_blockSize(blockSize), m_blockNum(blockNum)
 {
     m_pool = new char[blockSize * blockNum];
-    m_allocTable = new bool[blockSize];
+    m_allocTable = new bool[blockNum];
     memset(m_allocTable, 0, blockNum * sizeof(bool));
 }
 
@@ -37,7 +37,7 @@ char* VolumeBlockAllocator::bmalloc()
     std::lock_guard<std::mutex> lk(m_mutex);
     for (int i = 0; i < m_blockNum; i++)
     {
-        if (m_allocTable[i]) {
+        if (!m_allocTable[i]) {
             m_allocTable[i] = true;
             return m_pool + (m_blockSize * i);
         }
