a6266534a98f918b199fdb77780d98fe52ce8474
XUranus <2257238649wdx@gmail.com>
2023-06-17 21:07:01 +0800

    update inc backup

--- main.cpp
+++ main.cpp
@@ -10,72 +10,34 @@
 
 using namespace volumebackup;
 
-// bool StartIncrementBackupTask(
-// 	const std::string& 	blockDevicePath,
-//     const std::string&	prevCopyDataDirPath,
-// 	const std::string&	prevCopyMetaDirPath,
-// 	const std::string&	outputCopyDataDirPath,
-// 	const std::string&	outputCopyMetaDirPath)
-// {
-    
-//     return true;
-// }
-
-// bool StartRestoreTask(
-// 	const std::string&	copyDataDirPath,
-// 	const std::string&	copyMetaDirPath,
-//     const std::string& 	blockDevicePath)
-// {
-//     return true;    
-// }
-
 void PrintHelp()
 {
-    std::cout << "vbkup -v volume -d datadir -m metadir" << std::endl;
+    std::cout << "vbkup -v volume -d datadir -m metadir [-p prevmetadir]" << std::endl;
 }
 
-int main(int argc, char** argv)
+int StartRestoreTask(
+	const std::string&	copyDataDirPath,
+	const std::string&	copyMetaDirPath,
+    const std::string& 	blockDevicePath)
 {
-    int ch;
-    std::string blockDevicePath = "";
-    std::string copyDataDirPath = "";
-    std::string copyMetaDirPath = "";
-    while ((ch = getopt(argc, argv, "v:d:m:h")) != -1) {
-        switch (ch) {
-            case 'v' : {
-                blockDevicePath = optarg;
-                break;
-            }
-            case 'd' : {
-                copyDataDirPath = optarg;
-                break;
-            }
-            case 'm' : {
-                copyMetaDirPath = optarg;
-                break;
-            }
-            case 'h' : {
-                PrintHelp();
-                return 0;
-            }
-        }
-    }
-    if (blockDevicePath.empty() || copyDataDirPath.empty() || copyMetaDirPath.empty()) {
-        PrintHelp();
-        return 1;
-    } 
-    std::cout << "blockDevicePath: " << blockDevicePath << std::endl;
-    std::cout << "copyDataDirPath: " << copyDataDirPath << std::endl;
-    std::cout << "copyMetaDirPath: " << copyMetaDirPath << std::endl;
+    // TODO::implement it!
+    std::cerr << "restore not implemented!" << std::endl;
+    return 0;    
+}
 
-    xuranus::minilogger::Logger::GetInstance().SetLogLevel(xuranus::minilogger::LoggerLevel::DEBUG);
-
+int ExecVolumeBackup(
+    CopyType copyType,
+    const std::string& blockDevicePath,
+    const std::string& prevCopyMetaDirPath,
+    const std::string& outputCopyDataDirPath,
+    const std::string& outputCopyMetaDirPath)
+{
     VolumeBackupConfig backupConfig {};
-    backupConfig.copyType = CopyType::FULL;
+    backupConfig.copyType = copyType;
     backupConfig.blockDevicePath = blockDevicePath;
-    backupConfig.prevCopyMetaDirPath = "";
-    backupConfig.outputCopyDataDirPath = copyDataDirPath;
-    backupConfig.outputCopyMetaDirPath = copyMetaDirPath;
+    backupConfig.prevCopyMetaDirPath = prevCopyMetaDirPath;
+    backupConfig.outputCopyDataDirPath = outputCopyDataDirPath;
+    backupConfig.outputCopyMetaDirPath = outputCopyMetaDirPath;
     backupConfig.blockSize = DEFAULT_BLOCK_SIZE;
     backupConfig.sessionSize = DEFAULT_SESSION_SIZE;
     backupConfig.hasherNum = DEFAULT_HASHER_NUM;
@@ -100,5 +62,56 @@ int main(int argc, char** argv)
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     std::cout << "volume backup task completed!" << std::endl;
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    int ch;
+    std::string blockDevicePath = "";
+    std::string copyDataDirPath = "";
+    std::string copyMetaDirPath = "";
+    std::string prevCopyMetaDirPath = "";
+    while ((ch = getopt(argc, argv, "v:d:m:p:h")) != -1) {
+        switch (ch) {
+            case 'v' : {
+                blockDevicePath = optarg;
+                break;
+            }
+            case 'd' : {
+                copyDataDirPath = optarg;
+                break;
+            }
+            case 'm' : {
+                copyMetaDirPath = optarg;
+                break;
+            }
+            case 'p' : {
+                prevCopyMetaDirPath = optarg;
+                break;
+            }
+            case 'h' : {
+                PrintHelp();
+                return 0;
+            }
+        }
+    }
+    if (blockDevicePath.empty() || copyDataDirPath.empty() || copyMetaDirPath.empty()) {
+        PrintHelp();
+        return 1;
+    } 
+    std::cout << "blockDevicePath: " << blockDevicePath << std::endl;
+    std::cout << "copyDataDirPath: " << copyDataDirPath << std::endl;
+    std::cout << "copyMetaDirPath: " << copyMetaDirPath << std::endl;
+    std::cout << "prevCopyMetaDirPath: " << prevCopyMetaDirPath << std::endl;
+
+    xuranus::minilogger::Logger::GetInstance().SetLogLevel(xuranus::minilogger::LoggerLevel::DEBUG);
+    if (prevCopyMetaDirPath.empty()) {
+        std::cout << "Doing full backup" << std::endl;
+        return ExecVolumeBackup(CopyType::FULL, blockDevicePath, prevCopyMetaDirPath, copyDataDirPath, copyMetaDirPath);
+    } else {
+        std::cout << "Doing increment backup" << std::endl;
+        return ExecVolumeBackup(CopyType::INCREMENT, blockDevicePath, prevCopyMetaDirPath, copyDataDirPath, copyMetaDirPath);
+    }
     return 0;
 }
