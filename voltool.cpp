3edd32c7c8a64a2bf907f362643c1bf13c441477
XUranus <2257238649wdx@gmail.com>
2023-07-03 05:13:55 +0800

    add voltool

--- voltool.cpp
+++ voltool.cpp
@@ -12,13 +12,51 @@
 
 #include "GetOption.h"
 #include "VolumeUtils.h"
+#include <iostream>
 
 using namespace volumeprotect;
 using namespace xuranus::getopt;
 
+void PrintHelp()
+{
+    std::cout << "usage: voltool -v [device path]" << std::endl;
+}
+
+void PrintVolumeInfo(const std::string& volumePath)
+{
+    std::cout << "UUID:  " << util::ReadVolumeUUID(volumePath) << std::endl;
+    std::cout << "Type:  " << util::ReadVolumeType(volumePath) << std::endl;
+    std::cout << "Label: " << util::ReadVolumeLabel(volumePath) << std::endl;
+
+    int partitionNumber = 0;
+    for (const auto& partition : util::ReadVolumePartitionTable(volumePath)) {
+        std::cout << "======= partition[" << ++partitionNumber << "] =======" << std::endl;
+        std::cout << "filesystem:       " << partition.filesystem << std::endl;
+        std::cout << "patitionNumber:   " << partition.patitionNumber << std::endl;
+        std::cout << "firstSector:      " << partition.firstSector << std::endl;
+        std::cout << "lastSector:       " << partition.lastSector << std::endl;
+        std::cout << "totalSectors:     " << partition.totalSectors << std::endl;
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, const char** argv)
 {
-    GetOptionResult result = GetOption(argv + 1, argc - 1, "v:d:m:p:h:r:l", {});
-
+    GetOptionResult result = GetOption(
+        argv + 1,
+        argc - 1,
+        "v:h",
+        { "volume=", "help" });
+    for (const OptionResult opt: result.opts) {
+        if (opt.option == "h" || opt.option == "help") {
+            PrintHelp();
+            return 0;
+        } else if (opt.option == "v" || opt.option == "volume") {
+            std::string volumePath = opt.value;
+            PrintVolumeInfo(volumePath);
+            return 0;
+        }
+    }
+    PrintHelp();
     return 0;
 }
