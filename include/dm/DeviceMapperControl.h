6f1243a2082ec442832f5cccb736bdfc6b4ee6aa
XUranus <2257238649wdx@gmail.com>
2023-08-10 21:54:17 +0800

    update loop device api

--- include/dm/DeviceMapperControl.h
+++ include/dm/DeviceMapperControl.h
@@ -1,10 +1,77 @@
 #ifndef VOLUMEBACKUP_DM_DEVICE_MAPPER_CONTROL_H
 #define VOLUMEBACKUP_DM_DEVICE_MAPPER_CONTROL_H
 
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
 namespace volumebackup {
 namespace devicemapper {
 
+/**
+ * reference url:
+ * https://android.googlesource.com/platform/system/core/+/refs/heads/main/fs_mgr/libdm/include/libdm/dm_target.h
+ */
+class DmTarget {
+public:
+    // Return the first logical sector represented by this target.
+    uint64_t    StartSector() const;
+    // Returns size in number of sectors when this target is part of a DmTable, return 0 otherwise.
+    uint64_t    SectorsCount() const;
 
+    // Function that converts this object to a string of arguments that can
+    // be passed to the kernel for adding this target in a table. Each target (e.g. verity, linear)
+    // must implement this, for it to be used on a device.
+    std::string Serialize() const;
+
+    // Return the name of the parameter
+    virtual std::string Name() const = 0;
+    
+    // Get the parameter string that is passed to the end of the dm_target_spec
+    // for this target type.
+    virtual std::string GetParameterString() const = 0;
+private:
+    uint64_t        m_startSector;
+    uint64_t        m_sectorsCount;
+};
+
+class DmTargetLinear final : public DmTarget {
+public:
+    std::string     BlockDevicePath() const;
+    uint64_t        PhysicalSector() const;
+
+    std::string GetParameterString() const override;
+    std::string Name() const override;
+private:
+    std::string     m_blockDevicePath;
+    uint64_t        m_physicalSector;
+};
+
+class DmTable {
+public:
+    bool    AddTarget(std::shared_ptr<DmTarget> target);
+    
+    // Returns the string represntation of the table that is ready to be passed into the kernel
+    // as part of the DM_TABLE_LOAD ioctl.
+    std::string Serialize() const;
+private:
+    // list of targets defined in this table sorted by their start and end sectors.
+    // Note: Overlapping targets MUST never be added in this list.
+    std::vector<std::shared_ptr<DmTarget>>  m_targets;
+
+    // Total size in terms of # of sectors, as calculated by looking at the last and the first
+    // target in 'm_targets'.
+    uint64_t    m_sectorsCount;
+};
+
+/*
+ * reference url:
+ * https://android.googlesource.com/platform/system/core/+/refs/heads/main/fs_mgr/libdm/dm.cpp
+ */
+class DeviceMapper {
+
+};
 
 }
 }
--- src/dm/DeviceMapperControl.cpp
+++ src/dm/DeviceMapperControl.cpp
@@ -1,3 +1,83 @@
 #include "DeviceMapperControl.h"
+#include <string>
+#include <linux/dm-ioctl.h>
 
-using namespace volumebackup::dm;
+using namespace volumebackup;
+using namespace volumebackup::devicemapper;
+
+namespace {
+    const std::string DEVICE_MAPPER_CONTROL_PATH = "/dev/mapper/control";
+};
+
+// implement DmTarget
+uint64_t DmTarget::StartSector() const
+{
+    return m_startSector;
+}
+
+uint64_t DmTarget::SectorsCount() const
+{
+    return m_sectorsCount;
+}
+
+std::string DmTarget::Serialize() const
+{
+    // Create a string containing a dm_target_spec, parameter data, and an
+    // explicit null terminator.
+    std::string data(sizeof(dm_target_spec), '\0');
+    data += GetParameterString();
+    data.push_back('\0');
+    // The kernel expects each target to be 8-byte aligned.
+    size_t padding = data.size() % 8;
+    for (size_t i = 0; i < padding; i++) {
+        data.push_back('\0');
+    }
+    // Finally fill in the dm_target_spec.
+    struct dm_target_spec* spec = reinterpret_cast<struct dm_target_spec*>(&data[0]);
+    spec->sector_start = StartSector();
+    spec->length = SectorsCount();
+    snprintf(spec->target_type, sizeof(spec->target_type), "%s", Name().c_str());
+    spec->next = (uint32_t)data.size();
+    return data;
+}
+
+// implement DmTargetLinear
+std::string DmTargetLinear::Name() const
+{
+    return "linear";
+}
+
+std::string DmTargetLinear::GetParameterString() const
+{
+    return m_blockDevicePath + " " + std::to_string(m_physicalSector);
+}
+
+std::string DmTargetLinear::BlockDevicePath() const
+{
+    return m_blockDevicePath;
+}
+
+uint64_t DmTargetLinear::PhysicalSector() const
+{
+    return m_physicalSector;
+}
+
+// implement DmTable
+bool DmTable::AddTarget(std::shared_ptr<DmTarget> target)
+{
+    // HINT:: check target valid
+    m_targets.push_back(target);
+    return true;
+}
+
+std::string DmTable::Serialize() const
+{
+    // HINT:: check target valid
+    std::string tableString;
+    for (const auto& target : m_targets) {
+        tableString += target->Serialize();
+    }
+    return tableString;
+}
+
+// implement DeviceMapper
